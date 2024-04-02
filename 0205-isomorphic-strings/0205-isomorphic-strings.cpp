14e7eb027a88e97d36da529bd227fd0ca53f2a00
Vrishank Kirpane <142775808+vrishank1234@users.noreply.github.com>
2024-04-02 16:04:28 +0530

    Time: 10 ms (20.25%), Space: 9.3 MB (63.23%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,26 +5,29 @@ bool isIsomorphic(string s, string t) {
     if (s.size() != t.size())
         return false;
 
-    int s_to_t[256] = {0}; // Mapping from characters in s to characters in t
-    int t_to_s[256] = {0}; // Mapping from characters in t to characters in s
+    unordered_map<char, char> s_to_t_map;
+    unordered_map<char, char> t_to_s_map;
 
     for (int i = 0; i < s.size(); ++i) {
         char char_s = s[i];
         char char_t = t[i];
 
-        // Check if the characters are already mapped differently
-        if (s_to_t[char_s] != 0 && s_to_t[char_s] != char_t)
+        // Check if the character from s is already mapped to a different character in t
+        if (s_to_t_map.find(char_s) != s_to_t_map.end() && s_to_t_map[char_s] != char_t)
             return false;
-        if (t_to_s[char_t] != 0 && t_to_s[char_t] != char_s)
+
+        // Check if the character from t is already mapped to a different character in s
+        if (t_to_s_map.find(char_t) != t_to_s_map.end() && t_to_s_map[char_t] != char_s)
             return false;
 
         // If the characters are not mapped yet, create the mapping
-        if (s_to_t[char_s] == 0)
-            s_to_t[char_s] = char_t;
-        if (t_to_s[char_t] == 0)
-            t_to_s[char_t] = char_s;
+        if (s_to_t_map.find(char_s) == s_to_t_map.end())
+            s_to_t_map[char_s] = char_t;
+        if (t_to_s_map.find(char_t) == t_to_s_map.end())
+            t_to_s_map[char_t] = char_s;
     }
 
     return true;
 }
+
 };
