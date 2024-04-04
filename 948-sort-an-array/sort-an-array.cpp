9012d798c555508188beda2692d5190781b101a9
Vrishank Kirpane <142775808+vrishank1234@users.noreply.github.com>
2024-04-04 11:54:47 +0530

    Time: 390 ms (32.39%) | Memory: 179.6 MB (24.87%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        vector<int> L(n1), R(n2);
+
+        for (int i = 0; i < n1; i++)
+            L[i] = nums[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = nums[mid + 1 + j];
+
+        int i = 0; 
+        int j = 0;
+        int k = left; 
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                nums[k] = L[i];
+                i++;
+            } else {
+                nums[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2; 
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
