ee73d519196f2bcb1da576dc5662df7ebf35c67e
Vrishank Kirpane <142775808+vrishank1234@users.noreply.github.com>
2025-01-25 11:33:02 +0530

    Time: 57 ms (91.34%) | Memory: 173.2 MB (63.29%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,80 @@
+
+class LRUCache {
+private:
+    struct Node {
+        int key, value;
+        Node* prev;
+        Node* next;
+        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+
+    int capacity;
+    std::unordered_map<int, Node*> cache; 
+    Node* head; 
+    Node* tail; 
+
+    void removeNode(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void addNodeToHead(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity) {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (cache.find(key) == cache.end()) {
+            return -1; 
+        }
+        Node* node = cache[key];
+        removeNode(node);
+        addNodeToHead(node);
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            removeNode(node);
+            addNodeToHead(node);
+        } else {
+            if (cache.size() == capacity) {
+                Node* lru = tail->prev;
+                removeNode(lru);
+                cache.erase(lru->key);
+                delete lru;
+            }
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addNodeToHead(newNode);
+        }
+    }
+
+    ~LRUCache() {
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
